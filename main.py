ee773036e0805e766c6e81bdb4f6bc4fa35d7b72
jacob-cantrell <jcantrell218@gmail.com>
2025-01-22 13:37:12 -0500

    CH:3 - Asteroids, L1: - Asteroids completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -8,6 +10,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     player = Player( (SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
